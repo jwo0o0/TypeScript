da2523c9cb6124dc73e0f16b9139d57a7f706eab
jwo0o0 <jwo0o0358@gmail.com>
2023-02-07 17:25:44 +0900

    section 2 10 ~ 19

--- Section_2/app.js
+++ Section_2/app.js
@@ -1,7 +1,18 @@
-function add(n1, n2) {
-    return n1 + n2;
+var person = {
+    name: "jwoo",
+    age: 24,
+    hobbies: ["cooking", "reading"],
+    role: [2, "author"]
+};
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var res = add(number1, number2);
-console.log(res);
+//객체 타입
+//키 - 타입 쌍
+//어딘가에서 사용되는 객체 타입을 설명하기 위해 작성되는 것
+//객체 타입의 타입스크립트 표현일 뿐 실제 객체가 아님
+//튜플
+//길이가 고정된 특수 배열, TS에서 추가됨
+//배열의 길이, 타입까지 미리 지정
--- Section_2/app.ts
+++ Section_2/app.ts
@@ -1,9 +1,26 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; //tuple type
+} = {
+  name: "jwoo",
+  age: 24,
+  hobbies: ["cooking", "reading"],
+  role: [2, "author"],
+};
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
 }
 
-const number1 = 5;
-const number2 = 2.8;
+//객체 타입
+//키 - 타입 쌍
+//어딘가에서 사용되는 객체 타입을 설명하기 위해 작성되는 것
+//객체 타입의 타입스크립트 표현일 뿐 실제 객체가 아님
 
-const res = add(number1, number2);
-console.log(res);
+//튜플
+//길이가 고정된 특수 배열, TS에서 추가됨
+//배열의 길이, 타입까지 미리 지정
