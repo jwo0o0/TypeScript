27846630e008ee594ae8d3922a35b30bd19ebe49
jwo0o0 <jwo0o0358@gmail.com>
2023-02-26 22:53:48 +0900

    Section5 63 ~ 68

--- Section_5/dist/app.js
+++ Section_5/dist/app.js
@@ -1,13 +1,23 @@
 "use strict";
 class Department {
     //생성자 함수
-    constructor(n) {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        // private id: string;
+        // private name: string; //public이 디폴트값
         this.employees = [];
-        this.name = n;
+        // this.id = id;
+        // this.name = n;
+        //동일한 이름으로 class가 만들어질 때 property를 생성
+    }
+    //readonly: TS에만 존재
+    static createEmployee(name) {
+        return { name: name };
     }
     //메서드
     describe() {
-        console.log("Department: " + this.name);
+        console.log(`Department (${this.id}): ${this.name}`);
     }
     addEmployee(employee) {
         this.employees.push(employee);
@@ -18,10 +28,10 @@ class Department {
     }
 }
 //es6 - Class 문법 지원
-const accounting = new Department("Accounting");
+const accounting = new Department("d1", "Accounting");
 accounting.addEmployee("jwoo");
 accounting.addEmployee("ellie");
-//not good - private 키워드로 보호
+//not good -> private 키워드로 보호
 //accounting.employees[2] = "anna";
 accounting.name = "NEW_NAME";
 //javascript - public, private를 구분하지 않음, TS만이 지원
@@ -30,3 +40,55 @@ accounting.printEmployeeInformation();
 // const accountingCopy = { name: "marketing", describe: accounting.describe };
 // accountingCopy.describe();
 //describe 함수 자체를 전달함
+//상속
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        //this 키워드 사용전에 호출해 주어야 함
+        super(id, "IT");
+        this.admins = admins;
+    }
+}
+const employee1 = Department.createEmployee("max");
+console.log(employee1);
+const it = new ITDepartment("d2", ["jwoo"]);
+it.describe();
+it.printEmployeeInformation();
+class AccountingDepartment extends Department {
+    //getter
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("No report found.");
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("not a valid value");
+        }
+        this.addReport(value);
+    }
+    constructor(id, reports) {
+        super(id, "Accounting");
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    //메서드 오버라이딩
+    addEmployee(name) {
+        if (name === "Max") {
+            return;
+        }
+        //private 프로퍼티 - 상속 받은 클래스에서 접근 불가
+        //protected 프로퍼티 - 상속 받은 클래스에서 접근 가능
+        this.employees.push(name);
+    }
+    addReport(text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+}
+const newAccounting = new AccountingDepartment("d2", []);
+//setter로 접근
+newAccounting.mostRecentReport = "year end report";
+newAccounting.addReport("report");
+//getter로 프로퍼티에 접근
+console.log(newAccounting.mostRecentReport);
--- Section_5/src/app.ts
+++ Section_5/src/app.ts
@@ -1,15 +1,23 @@
 class Department {
-  public name: string; //public이 디폴트값
-  private employees: string[] = [];
+  // private id: string;
+  // private name: string; //public이 디폴트값
+  protected employees: string[] = [];
 
   //생성자 함수
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+    //동일한 이름으로 class가 만들어질 때 property를 생성
+  }
+  //readonly: TS에만 존재
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   //메서드
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -21,12 +29,12 @@ class Department {
 }
 //es6 - Class 문법 지원
 
-const accounting = new Department("Accounting");
+const accounting = new Department("d1", "Accounting");
 
 accounting.addEmployee("jwoo");
 accounting.addEmployee("ellie");
 
-//not good - private 키워드로 보호
+//not good -> private 키워드로 보호
 //accounting.employees[2] = "anna";
 accounting.name = "NEW_NAME";
 
@@ -38,3 +46,65 @@ accounting.printEmployeeInformation();
 // const accountingCopy = { name: "marketing", describe: accounting.describe };
 // accountingCopy.describe();
 //describe 함수 자체를 전달함
+
+//상속
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    //this 키워드 사용전에 호출해 주어야 함
+    super(id, "IT");
+  }
+}
+
+const employee1 = Department.createEmployee("max");
+console.log(employee1);
+
+const it = new ITDepartment("d2", ["jwoo"]);
+it.describe();
+it.printEmployeeInformation();
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  //getter
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value) {
+    if (!value) {
+      throw new Error("not a valid value");
+    }
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  //메서드 오버라이딩
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    //private 프로퍼티 - 상속 받은 클래스에서 접근 불가
+    //protected 프로퍼티 - 상속 받은 클래스에서 접근 가능
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+}
+
+const newAccounting = new AccountingDepartment("d2", []);
+
+//setter로 접근
+newAccounting.mostRecentReport = "year end report";
+newAccounting.addReport("report");
+//getter로 프로퍼티에 접근
+console.log(newAccounting.mostRecentReport);
